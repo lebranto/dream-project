4717006610cf50b00feb4c3eb0e17521e7da7ba1
DESKTOP-GMG48RP\user1 <lebranto@gamil.com>
2026-03-23 15:32:01 +0900

    구매내역과 최근 본 상품 리스트 불러오기 코드 작업과 orders 객체 수정

--- jipshop/src/main/java/com/kh/jipshop/mypage/controller/MypageController.java
+++ jipshop/src/main/java/com/kh/jipshop/mypage/controller/MypageController.java
@@ -9,33 +9,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MypageController {
 
 	@GetMapping("/purchase")
+<<<<<<< HEAD
+	public String purchase() {
+		System.out.println("구매 내역");
+			return "mypage/purchase";
+=======
 	public String orderList() {
 		
 			return "mypage/order";
+>>>>>>> parent of 2ca52f8 (초반과정)
 	}
 	
 	@GetMapping("/recent")
 	public String recent() {
+<<<<<<< HEAD
+		System.out.println("최근 본 상품");
+=======
 		
+>>>>>>> parent of 2ca52f8 (초반과정)
 			return "mypage/recent";
 	}
 	
 	@GetMapping("/inquiry")
 	public String inquiry() {
-	
+		System.out.println("문의 내역");
 			return "mypage/inquiry";
 	}
 	
 	@GetMapping("/updateMemberCheck")
 	public String updateMemberCheck() {
-		
+		System.out.println("확인 화면");
 			return "mypage/updateMemberCheck";
 	}
 	
 	
 	@GetMapping("/memberDelete")
 	public String memberDelete() {
-		
+		System.out.println("확인 화면");
 		return "mypage/memberDelete";
 	}
 	
